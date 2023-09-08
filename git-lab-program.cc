eccf29418d7519da79c76b7dbc82a3eba985a51d
Brayden Graves <bg069023@ohio.edu>
2023-09-08 17:51:07 -0400

    Update

---
+++ git-lab-program.cc
@@ -0,0 +1,17 @@
+/*
+ *       File: git-lab-program.cc   
+ *     Author: Brayden Graves
+ *       Date: 9/8/2023
+ *Description: Test file for Lab2
+ */
+
+ #include <iostream>
+ #include <iomanip>
+ #include <cstdlib>
+using namespace std;
+
+int main(int argc, char const *agrv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}
